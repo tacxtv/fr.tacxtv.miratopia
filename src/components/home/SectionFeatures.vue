6e315e1130245cb7654a819307d552d8fb23ec32
Samsaoule <samuel034bt@gmail.com>
2025-04-28 21:28:45 +0200

    add shop button link

--- src/components/home/SectionFeatures.vue
+++ src/components/home/SectionFeatures.vue
@@ -54,6 +54,10 @@ onMounted(async () => {
 const openGuide = () => {
   window.open('https://tacx.notion.site/Wikitopia-1de153ee963f80d29df1cdeeb85779e5', '_blank')
 }
+
+const redirectToShop = () => {
+  window.open('https://mirashop.tacxtv.fr/bank/upload', '_blank')
+}
 </script>
 
 <template>
@@ -75,7 +79,7 @@ const openGuide = () => {
               Profitez d'un <b>système d'économie in-game complet</b> et développez votre économie personnelle pour pouvoir acheter
               des biens et des services.
             </div>
-            <button class="btn btn-primary px-4 py-2 rounded-md font-medium transition-all bg-[var(--primary-0)] text-[var(--surface-0)] hover:bg-[var(--primary-10)] cursor-pointer">
+            <button class="btn btn-primary px-4 py-2 rounded-md font-medium transition-all bg-[var(--primary-0)] text-[var(--surface-0)] hover:bg-[var(--primary-10)] cursor-pointer" @click="redirectToShop">
               <span class="flex items-center gap-2">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
